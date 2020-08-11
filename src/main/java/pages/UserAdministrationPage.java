0ed235861557aa980b407334c01fcae168fa62f4
andrei.kuturmazov <andrei.kuturmazov@gmail.com>
2020-08-11 20:47:26 +0300

    adding user adding form check

--- src/main/java/pages/UserAdministrationPage.java
+++ src/main/java/pages/UserAdministrationPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import utils.Property;
 
@@ -7,14 +8,22 @@ import static com.codeborne.selenide.Selenide.$x;
 
 public class UserAdministrationPage {
 
-    private final SelenideElement employeeNameField = $x("//input[@id='systemUser_employeeName_empName']");
+    private final SelenideElement formHeader = $x("//h1[@id='UserHeading']");
+    private final SelenideElement userRoleLabel = $x("//label[contains(text(), 'User Role')]");
+    private final SelenideElement userRoleDropDown = $x("//select[@id='systemUser_userType']");
+    private final SelenideElement employeeLabel = $x("//label[contains(text(), 'Employee Name')]");
+    private final SelenideElement employeeField = $x("//input[@id='systemUser_employeeName_empName']");
+    private final SelenideElement employeeFieldValidationMessage = $x("//span[@for='systemUser_employeeName_empName']");
+    private final SelenideElement userNameLabel = $x("//label[contains(text(), 'Username')]");
     private final SelenideElement userNameField = $x("//input[@id='systemUser_userName']");
+    private final SelenideElement userNameFieldValidationMessage = $x("//span[@for='systemUser_userName']");
+    private final SelenideElement statusLabel = $x("//label[contains(text(), 'Status')]");
     private final SelenideElement formSaveButton = $x("//input[@id='btnSave']");
     private final SelenideElement addButton = $x("//div[@class='top']/input[@type='button']");
     private final SelenideElement successMessage = $x("//div[@class='fadable']");
 
     public void fillUserForm() {
-        employeeNameField.sendKeys(Property.getProperty("employeeName"));
+        employeeField.sendKeys(Property.getProperty("employeeName"));
         userNameField.sendKeys(Property.getProperty("userName"));
     }
 
@@ -28,6 +37,23 @@ public class UserAdministrationPage {
 
     public void addUser() {
         addButton.click();
+    }
+
+    public void checkValidationMessage() {
+        formSaveButton.click();
+        employeeFieldValidationMessage.isDisplayed();
+        userNameFieldValidationMessage.isDisplayed();
+    }
+
+    public void checkFormElements() {
+        formHeader.shouldBe(Condition.visible);
+        userRoleLabel.shouldBe(Condition.visible);
+        userRoleDropDown.shouldBe(Condition.visible);
+        employeeLabel.shouldBe(Condition.visible);
+        employeeField.shouldBe(Condition.visible);
+        userNameLabel.shouldBe(Condition.visible);
+        userNameField.shouldBe(Condition.visible);
+        statusLabel.shouldBe(Condition.visible);
     }
 
     public void fillUserCreationFormAndSubmit() throws InterruptedException {
