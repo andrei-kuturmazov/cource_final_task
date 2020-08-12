71481b5a7afe48a8c155094e4cbd146dfc44682a
andrei.kuturmazov <andrei.kuturmazov@gmail.com>
2020-08-12 15:34:50 +0300

    adding dashboard elements check

--- src/main/java/pages/DashboardPage.java
+++ src/main/java/pages/DashboardPage.java
@@ -8,6 +8,9 @@ import static com.codeborne.selenide.Selenide.$x;
 public class DashboardPage {
 
     private final SelenideElement dashBoardTabLink = $x("//a[@id='menu_dashboard_index']");
+    private final SelenideElement assignLeaveLink = $x("//span[contains(text(), 'Assign Leave')]/parent::a/parent::div");
+    private final SelenideElement leaveListLink = $x("//span[contains(text(), 'Leave List')]/parent::a/parent::div");
+    private final SelenideElement timeSheetLink = $x("//span[contains(text(), 'Timesheets')]/parent::a/parent::div");
     private final SelenideElement dashBoardTitle = $x("//h1[contains(text(), 'Dashboard')]");
     private final SelenideElement dashBoardDiagram = $x("//div[@id='dashboard__employeeDistribution']");
     private final SelenideElement dashBoardLegend = $x("//div[@id='panel_draggable_1_1']");
@@ -19,6 +22,17 @@ public class DashboardPage {
 
     public void checkDashboardElements() {
         dashBoardTitle.shouldBe(Condition.visible);
+        checkQuickLaunchElements();
+        checkEmployeeDistributionElements();
+    }
+
+    public void checkQuickLaunchElements() {
+        assignLeaveLink.shouldBe(Condition.visible);
+        leaveListLink.shouldBe(Condition.visible);
+        timeSheetLink.shouldBe(Condition.visible);
+    }
+
+    public void checkEmployeeDistributionElements() {
         dashBoardDiagram.shouldBe(Condition.visible);
         dashBoardLegend.shouldBe(Condition.visible);
         dashBoardPendingRequests.shouldBe(Condition.visible);
