5983f535254f6c2831437cbcae4601df081442ce
andrei.kuturmazov <andrei.kuturmazov@gmail.com>
2020-08-12 15:02:17 +0300

    dashboard title adding

--- src/main/java/pages/DashboardPage.java
+++ src/main/java/pages/DashboardPage.java
@@ -8,6 +8,7 @@ import static com.codeborne.selenide.Selenide.$x;
 public class DashboardPage {
 
     private final SelenideElement dashBoardTabLink = $x("//a[@id='menu_dashboard_index']");
+    private final SelenideElement dashBoardTitle = $x("//h1[contains(text(), 'Dashboard')]");
     private final SelenideElement dashBoardDiagram = $x("//div[@id='dashboard__employeeDistribution']");
     private final SelenideElement dashBoardLegend = $x("//div[@id='panel_draggable_1_1']");
     private final SelenideElement dashBoardPendingRequests = $x("//div[@id='task-list-group-panel-menu_holder']");
@@ -17,6 +18,7 @@ public class DashboardPage {
     }
 
     public void checkDashboardElements() {
+        dashBoardTitle.shouldBe(Condition.visible);
         dashBoardDiagram.shouldBe(Condition.visible);
         dashBoardLegend.shouldBe(Condition.visible);
         dashBoardPendingRequests.shouldBe(Condition.visible);
