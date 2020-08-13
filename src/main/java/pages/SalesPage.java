a6c21bb2b0e75f620b299b564868972b90633a0b
andrei.kuturmazov <andrei.kuturmazov@gmail.com>
2020-08-13 09:22:12 +0300

    adding gender info

--- src/main/java/pages/SalesPage.java
+++ src/main/java/pages/SalesPage.java
@@ -17,6 +17,10 @@ public class SalesPage {
     private final SelenideElement otherIdField = $x("//input[@id='personal_txtOtherID']");
     private final SelenideElement driversLicenseField = $x("//input[@id='personal_txtLicenNo']");
     private final SelenideElement driversLicenseExpireDateField = $x("//input[@id='personal_txtLicExpDate']");
+    private final SelenideElement firstGenderRadioButton = $x("//input[@id='personal_optGender_1']");
+    private final SelenideElement firstGenderDescription = $x("//input[@id='personal_optGender_1']/following-sibling::label");
+    private final SelenideElement secondGenderRadioButton = $x("//input[@id='personal_optGender_2']");
+    private final SelenideElement secondGenderDescription = $x("//input[@id='personal_optGender_2']/following-sibling::label");
     private final SelenideElement martialStatusDropdown = $x("//select[@id='personal_cmbMarital']");
     private final SelenideElement nationalityDropDown = $x("//select[@id='personal_cmbNation']");
     private final SelenideElement dateOfBirth = $x("//input[@id='personal_DOB']");
@@ -53,10 +57,23 @@ public class SalesPage {
                 "Last name: %s, \n" +
                 "Middle name: %s, \n" +
                 "Employee id: %s \n" +
+                "Employee gender: %s \n" +
                 "Nationality: %s \n" +
                 "Date of birth: %s",
                 firstNameField.getValue(), lastNameField.getValue(),
-                middleNameField.getValue(), employeeIdField.getValue(),
-                nationalityDropDown.getValue(), dateOfBirth.getValue()));
+                middleNameField.getValue(), employeeIdField.getValue(), getGenderInfo(),
+                nationalityDropDown.getText(), dateOfBirth.getValue()));
+    }
+
+    public String getGenderInfo() {
+        if (firstGenderRadioButton.isSelected()) {
+            return firstGenderDescription.getText();
+        }
+        else if (secondGenderRadioButton.isSelected()) {
+            return secondGenderDescription.getText();
+        }
+        else {
+            return null;
+        }
     }
 }
